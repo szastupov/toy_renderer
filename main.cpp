a8994f80488be84c8de33c97441f8b64849e79d9
Stepan Zastupov <redchrom@gmail.com>
2010-06-30 01:40:50 +0400

    Better line drawing

--- main.cpp
+++ main.cpp
@@ -8,21 +8,25 @@
 #include "transform.h"
 #include "SDL.h"
 
+#define SWAP(x, y) { x ^= y; y ^= x; x ^= y; }
+
 class Renderer
 {
     SDL_Surface *m_screen;
     std::vector<vec4f> dots;
     Matrix4f m_viewport;
     Matrix4f m_model;
+    uint32_t m_color;
 public:
     Renderer(SDL_Surface *screen)
         : m_screen(screen)
     {
         float sx = m_screen->w/2;
         float sy = m_screen->h/2;
+        setColor(0xFF, 0x00, 0x00);
 
         m_viewport = scale(sx, -sy, 1.0f) * translate(1.0f, -1.0f, 0.0f);
-        m_model = rotate(1.f, 1.f, 0.f, 0.f);
+//        m_model = rotate(1.f, 1.f, 0.f, 0.f);
 
         // Example data
         float pp[][3] = {
@@ -43,13 +47,43 @@ public:
             if (dot.x() < 0 || dot.x() > m_screen->w ||
                 dot.y() < 0 || dot.y() > m_screen->h)
                 continue;
-            setPixel(dot.x(), dot.y(), 0xFF, 0x00, 0x00);
+            plot(dot.x(), dot.y());
+        }
+    }
+
+    void bLine(int x1, int y1, int x2, int y2)
+    {
+        bool steep = abs(y2 - y1) > abs(x2 - x1);
+        if (steep) {
+            SWAP(x1, y1);
+            SWAP(x2, y2);
+        }
+        if (x1 > x2) {
+            SWAP(x1, x2);
+            SWAP(y1, y2);
+        }
+
+        int dx = x2-x1,
+            dy = abs(y2-y1),
+            error = dx >> 1,
+            ystep = y1 < y2 ? 1 : -1;
+
+        for (int x = x1, y = y1; x <= x2; x++) {
+            if (steep)
+                plot(y, x);
+            else
+                plot(x, y);
+            error -= dy;
+            if (error < 0) {
+                y += ystep;
+                error += dx;
+            }
         }
     }
 
     void drawLines(const Matrix4f &trans)
     {
-        float x1, y1, x2, y2;
+        int x1, y1, x2, y2;
         for (unsigned i = 0; i < dots.size(); i++) {
             vec4f dot = trans * dots[i];
             dot = dot/dot.w();
@@ -60,23 +94,13 @@ public:
             if (i == 0) {
                 x1 = dot[0];
                 y1 = dot[1];
-            } else {
-                x2 = dot[0];
-                y2 = dot[1];
-                float dx = x2-x1;
-                float dy = y2-y1;
-
-                if (dx == 0) {
-                    for (float iy = y1; iy <= y2; iy++)
-                        setPixel(x1, iy, 0xFF, 0x00, 0x00);
-                } else {
-                    for (float x = x1; x <= x2; x++) {
-                        float y = y1 + (dy) * (x - x1)/dx;
-                        setPixel(x, y, 0xFF, 0x00, 0x00);
-                    }
-                }
-                x1 = x2; y1 = y2;
+                continue;
             }
+
+            x2 = dot[0];
+            y2 = dot[1];
+            bLine(x1, y1, x2, y2);
+            x1 = x2; y1 = y2;
         }
     }
 
@@ -95,14 +119,20 @@ public:
         SDL_Flip(m_screen);
     }
 
-    inline void setPixel(unsigned x, unsigned y, uint8_t r, uint8_t g, uint8_t b)
+    inline void plot(unsigned x, unsigned y)
     {
         SDL_PixelFormat *format = m_screen->format;
         int bpp = format->BytesPerPixel;
         int pitch = m_screen->pitch;
         uint8_t *pixels = (uint8_t*)m_screen->pixels + y*pitch + x*bpp;
 
-        *(uint32_t*)pixels = r << format->Rshift | g << format->Gshift | b << format->Bshift;
+        *(uint32_t*)pixels = m_color;
+    }
+
+    void setColor(uint8_t r, uint8_t g, uint8_t b)
+    {
+        SDL_PixelFormat *format = m_screen->format;
+        m_color = r << format->Rshift | g << format->Gshift | b << format->Bshift;
     }
 
 };
